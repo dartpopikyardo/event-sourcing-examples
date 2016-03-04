5a3f01e9a5ab136e61354ee59f7edad403f5380b
dartpopikyardo <stanislav.semichevskiy@digitalarrowtech.com>
2016-03-04 22:45:35 +0300

    zuul-api-gateway initial commit

--- java-spring/api-gateway-service/src/main/java/net/chrisrichardson/eventstore/javaexamples/banking/apigateway/main/ApiGatewayServiceMain.java
+++ java-spring/api-gateway-service/src/main/java/net/chrisrichardson/eventstore/javaexamples/banking/apigateway/main/ApiGatewayServiceMain.java
@@ -1,13 +1,16 @@
 package net.chrisrichardson.eventstore.javaexamples.banking.apigateway.main;
 
-import net.chrisrichardson.eventstore.javaexamples.banking.apigateway.ApiGatewayServiceConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * Created by Main on 19.01.2016.
  */
+@SpringBootApplication
+@EnableZuulProxy
 public class ApiGatewayServiceMain {
     public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayServiceConfiguration.class, args);
+        SpringApplication.run(ApiGatewayServiceMain.class, args);
     }
 }
